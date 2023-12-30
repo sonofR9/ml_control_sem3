3f7b150b0484d9df3cdc882af095a0b4ac8b7932
sonofR9 <sasha_7tdd7@mail.ru>
2023-12-30 21:31:08 +0300

    REF cmake

--- cmake/get_boost_lib.cmake
+++ cmake/get_boost_lib.cmake
@@ -1,26 +1,27 @@
-find_package(Boost 1.84 CONFIG
-    COMPONENTS program_options
-)
+function(find_boost_libs LIBRARIES)
+  # Find Boost using find_package
+  find_package(Boost 1.84 CONFIG COMPONENTS ${LIBRARIES})
 
-if(${Boost_FOUND})
-else()
-    set(BOOST_INCLUDE_LIBRARIES program_options)
-    set(BOOST_ENABLE_CMAKE ON)
+  # If Boost was found, return
+  if(Boost_FOUND)
+    return()
+  endif()
 
-    include(FetchContent)
-    FetchContent_Declare(Boost
-        URL https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.7z
-        USES_TERMINAL_DOWNLOAD TRUE 
-        GIT_PROGRESS TRUE   
-        DOWNLOAD_NO_EXTRACT FALSE
-    )
-    FetchContent_MakeAvailable(Boost)
+  # Otherwise, fetch Boost using FetchContent
+  set(BOOST_INCLUDE_LIBRARIES ${LIBRARIES})
+  set(BOOST_ENABLE_CMAKE ON)
 
-    if(Boost_INCLUDE_DIRS STREQUAL "Boost_INCLUDE_DIR-NOTFOUND")
-        set(Boost_INCLUDE_DIRS "${PROJECT_BINARY_DIR}/_deps/boost-build/libs")
-    endif()
+  include(FetchContent)
+  FetchContent_Declare(Boost
+    URL https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.7z
+    USES_TERMINAL_DOWNLOAD TRUE
+    GIT_PROGRESS TRUE
+    DOWNLOAD_NO_EXTRACT FALSE
+  )
+  FetchContent_MakeAvailable(Boost)
 
-    # set(Boost_LIBRARIES
-    #     "Boost::program_options"
-    # )
-endif()
+  # Ensure Boost_INCLUDE_DIRS is set correctly
+  if(Boost_INCLUDE_DIRS STREQUAL "Boost_INCLUDE_DIR-NOTFOUND")
+    set(Boost_INCLUDE_DIRS "${PROJECT_BINARY_DIR}/_deps/boost-build/libs")
+  endif()
+endfunction()
