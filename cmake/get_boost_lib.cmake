793cc598b381ed80850c4055624019124d3b8673
sonofR9 <sasha_7tdd7@mail.ru>
2023-12-30 13:50:51 +0300

    ADD boost download

--- cmake/get_boost_lib.cmake
+++ cmake/get_boost_lib.cmake
@@ -1,19 +1,26 @@
-# find third party libraries (boost:: beast, asio, system)
-find_package(Boost 1.74)
+find_package(Boost 1.84 CONFIG
+    COMPONENTS program_options
+)
 
 if(${Boost_FOUND})
 else()
-    include(FetchContent)
-    FetchContent_Declare(boost URL https://github.com/boostorg/boost/releases/download/boost-1.82.0/boost-1.82.0.tar.xz)
-    FetchContent_MakeAvailable(boost)
+    set(BOOST_INCLUDE_LIBRARIES program_options)
+    set(BOOST_ENABLE_CMAKE ON)
 
-    if(${Boost_INCLUDE_DIRS} STREQUAL "Boost_INCLUDE_DIR-NOTFOUND")
+    include(FetchContent)
+    FetchContent_Declare(Boost
+        URL https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.7z
+        USES_TERMINAL_DOWNLOAD TRUE 
+        GIT_PROGRESS TRUE   
+        DOWNLOAD_NO_EXTRACT FALSE
+    )
+    FetchContent_MakeAvailable(Boost)
+
+    if(Boost_INCLUDE_DIRS STREQUAL "Boost_INCLUDE_DIR-NOTFOUND")
         set(Boost_INCLUDE_DIRS "${PROJECT_BINARY_DIR}/_deps/boost-build/libs")
     endif()
 
-    set(Boost_LIBRARIES
-        "Boost::system"
-        "Boost::asio"
-        "Boost::beast"
-    )
+    # set(Boost_LIBRARIES
+    #     "Boost::program_options"
+    # )
 endif()
