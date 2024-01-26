5a69364d5819fdb49f52c31e306e0c0d5871b50a
sonofR9 <sasha_7tdd7@mail.ru>
2024-01-26 21:51:53 +0300

    REF forward kinematics: return all links positions

--- src/lbr-iiwa/lbr-iiwa-forward-kinematics.h
+++ src/lbr-iiwa/lbr-iiwa-forward-kinematics.h
@@ -24,6 +24,8 @@ using namespace optimization;
 template <typename T, template <typename> class Alloc>
 using Tensor2d = Tensor<Tensor<T, Alloc<T>>, Alloc<Tensor<T, Alloc<T>>>>;
 
+// NOLINTBEGIN(readability-identifier-naming)
+
 // ------------------------readable form of functions---------------------------
 template <typename T, template <typename> class Alloc>
 constexpr Tensor2d<T, Alloc> Tx(T linkLength) {
@@ -189,50 +191,97 @@ constexpr Tensor2d<T, Alloc>& RzO(Tensor2d<T, Alloc>& tensor, T q) {
   return tensor;
 };
 
-template <typename T, class Alloc>
-constexpr Tensor<T, Alloc> FK(const Tensor<T, Alloc>& q) {
-  static Tensor<T, Alloc> link_lengths{0.1, 0.2, 0.3, 0.4, 0.5, 0.6};
+// NOLINTEND(readability-identifier-naming)
 
-  auto d0{link_lengths[0]};
-  auto d1{link_lengths[1]};
-  auto d2{link_lengths[2]};
-  auto d3{link_lengths[3]};
-  auto d4{link_lengths[4]};
-  auto d6{link_lengths[5]};
-  auto q0{q[0]};
-  auto q1{q[1]};
-  auto q2{q[2]};
-  auto q3{q[3]};
-  auto q4{q[4]};
-  auto q5{q[5]};
-  auto q6{q[6]};
+// NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers)
+template <typename T, template <typename> class Alloc>
+class KukaLbrIiwa {
+ public:
+  /// @brief xyz, ypr
+  using Coordinates3d = Tensor<T, Alloc<T>>;
+  constexpr static Tensor<Coordinates3d, Alloc<Coordinates3d>> operator()(
+      const Tensor<T, Alloc<T>>& q) {
+    const auto& d0{kLinkLengths[0]};
+    const auto& d1{kLinkLengths[1]};
+    const auto& d2{kLinkLengths[2]};
+    const auto& d3{kLinkLengths[3]};
+    const auto& d4{kLinkLengths[4]};
+    const auto& d6{kLinkLengths[5]};
+    const auto& q0{q[0]};
+    const auto& q1{q[1]};
+    const auto& q2{q[2]};
+    const auto& q3{q[3]};
+    const auto& q4{q[4]};
+    const auto& q5{q[5]};
+    const auto& q6{q[6]};
 
-  // readable form:
-  // auto tBuf{Ty(q0)};
-  // tBuf = tBuf * Tz(d0);
-  // tBuf = tBuf * Rz(q1) * Tx(d1);
-  // tBuf = tBuf * Ry(q2) * Tx(d2);
-  // tBuf = tBuf * Ry(q3) * Tx(d3) * Tz(d4);
-  // tBuf = tBuf * Rx(q4) * Ry(q5) * Rx(q6);
-  // tBuf = tBuf * Tx(d6);
-  // optimized form:
-  static thread_local auto tBuf{Ty(q0)};
-  tBuf = Ty(q0);
-  TzO(tBuf, d0);
-  TxO(RzO(tBuf, q1), d1);
-  TxO(RyO(tBuf, q2), d2);
-  TzO(TxO(RyO(tBuf, q3), d3), d4);
-  RxO(RyO(RxO(tBuf, q4), q5), q6);
-  TxO(tBuf, d6);
+    // readable form:
+    // T1{Ty(q0)};
+    // T2 = T1 * Tz(d0);
+    // T3 = T2 * Rz(q1) * Tx(d1);
+    // T4 = T3 * Ry(q2) * Tx(d2);
+    // T5 = T4 * Ry(q3) * Tx(d3) * Tz(d4);
+    // T6 = T5 * Rx(q4) * Ry(q5) * Rx(q6);
+    // T7 = T6 * Tx(d6);
+    // optimized form:
+    static thread_local auto T01 =
+        Tensor2d<T, Alloc>(4, Tensor<T, Alloc<T>>(4));
+    static thread_local auto T02 =
+        Tensor2d<T, Alloc>(4, Tensor<T, Alloc<T>>(4));
+    static thread_local auto T03 =
+        Tensor2d<T, Alloc>(4, Tensor<T, Alloc<T>>(4));
+    static thread_local auto T04 =
+        Tensor2d<T, Alloc>(4, Tensor<T, Alloc<T>>(4));
+    static thread_local auto T05 =
+        Tensor2d<T, Alloc>(4, Tensor<T, Alloc<T>>(4));
+    static thread_local auto T06 =
+        Tensor2d<T, Alloc>(4, Tensor<T, Alloc<T>>(4));
+    static thread_local auto T07 =
+        Tensor2d<T, Alloc>(4, Tensor<T, Alloc<T>>(4));
+    T01 = Ty(q0);
+    T02 = T01;
+    TzO(T02, d0);
+    T03 = T02;
+    TxO(RzO(T03, q1), d1);
+    T04 = T03;
+    TxO(RyO(T04, q2), d2);
+    T05 = T04;
+    TzO(TxO(RyO(T05, q3), d3), d4);
+    T06 = T05;
+    RxO(RyO(RxO(T06, q4), q5), q6);
+    T07 = T06;
+    TxO(T07, d6);
 
-  return {
-      tBuf[0][3],
-      tBuf[1][3],
-      tBuf[2][3],
-      std::atan2(tBuf[2][0], tBuf[2][1]),
-      std::atan2(tBuf[0][2], -tBuf[1][2]),
-      std::atan2(std::sqrt(tBuf[0][2] * tBuf[0][2] + tBuf[1][2] * tBuf[1][2]),
-                 tBuf[2][2])};
-}
+    auto Coordinates3dFromTransform =
+        [](const Tensor2d<T, Alloc>& transform) -> Coordinates3d {
+      return {transform[0][3],
+              transform[1][3],
+              transform[2][3],
+              std::atan2(transform[2][0], transform[2][1]),
+              std::atan2(transform[0][2], -transform[1][2]),
+              std::atan2(std::sqrt(transform[0][2] * transform[0][2] +
+                                   transform[1][2] * transform[1][2]),
+                         transform[2][2])};
+    };
+    return {Coordinates3dFromTransform(T01), Coordinates3dFromTransform(T02),
+            Coordinates3dFromTransform(T03), Coordinates3dFromTransform(T04),
+            Coordinates3dFromTransform(T05), Coordinates3dFromTransform(T06),
+            Coordinates3dFromTransform(T07)};
+  }
+
+  constexpr static Tensor<Coordinates3d, Alloc<Coordinates3d>>
+  forwardKinematics(const Tensor<T, Alloc<T>>& q) {
+    return operator()(q);
+  }
+
+  template <int I>
+  constexpr static Coordinates3d getLinkPosition(const Tensor<T, Alloc<T>>& q) {
+    return forwardKinematics(q)[I];
+  }
+
+ private:
+  static const Tensor<T, Alloc<T>> kLinkLengths{0.1, 0.2, 0.3, 0.4, 0.5, 0.6};
+};
+// NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
 
 }  //  namespace kuka
